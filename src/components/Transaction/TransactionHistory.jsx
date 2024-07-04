5214a20ca1ba4b4637ea6b18bca48c31edc93445
Marisha9182 <120337365+Marisha9182@users.noreply.github.com>
2024-07-05 00:27:11 +0200

    css transaction

--- src/components/Transaction/TransactionHistory.jsx
+++ src/components/Transaction/TransactionHistory.jsx
@@ -1,18 +1,20 @@
+import '../Transaction/transactionStyle.css';
+
 const TransactionHistory = ( {items} ) => {
   return (
-    <table>
+    <table className='table'>
       <thead>
-        <tr>
+        <tr className='titleTable'>
           <th>Type</th>
           <th>Amount</th>
           <th>Currency</th>
         </tr>
       </thead>
 
-      <tbody>
+      <tbody className='tablesInformation'>
           {items.map((item) => (
             <tr key = {item.id}>
-              <td>{item.type}</td>
+              <td className='item-type'>{item.type}</td>
               <td>{item.amount}</td>
               <td>{item.currency}</td>
             </tr>

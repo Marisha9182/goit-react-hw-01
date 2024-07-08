aa3784558c4814c6f03e67cfb6f93b7f8a36c4c5
Marisha9182 <120337365+Marisha9182@users.noreply.github.com>
2024-07-08 14:28:50 +0200

    update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,42 @@
-import style from '../Profile/profileStyle.module.css'
+import style from "../Profile/profileStyle.module.css";
 
-const Profile = ({name, tag, location, image, stats:{followers, views, likes}}) => {
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={style.profileBox}>
+      <div>
+        <img
+          className={style.profileImg}
+          src={image}
+          alt="User avatar"
+          width="150"
+        />
+        <p className={style.name}>{name}</p>
+        <p className={style.info}>@{tag}</p>
+        <p className={style.info}>{location}</p>
+      </div>
 
-    return(
-        <div className={style.profileBox}>
-  <div>
-    <img className={style.profileImg}
-      src={image}
-      alt="User avatar"
-    width = '150'
-    />
-    <p className={style.name}>{name}</p>
-    <p className={style.info}>@{tag}</p>
-    <p className={style.info}>{location}</p>
-  </div>
-
-  <ul className={style.container}>
-    <li>
-      <span>Followers</span>
-      <span className={style.number}>{followers}</span>
-    </li>
-    <li>
-      <span>Views</span>
-      <span className={style.number}>{views}</span>
-    </li>
-    <li>
-      <span>Likes</span>
-      <span className={style.number}>{likes}</span>
-    </li>
-  </ul>
-</div>
-
-    )
-}
+      <ul className={style.container}>
+        <li>
+          <span>Followers</span>
+          <span className={style.number}>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span className={style.number}>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span className={style.number}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Profile;
